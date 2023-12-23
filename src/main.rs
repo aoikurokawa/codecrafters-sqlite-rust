60edbb63b7f1b3a989af2d895d34059a20be09c9
aoikurokawa <ht.so.ik0102@gmail.com>
2023-12-23 15:45:58 +0900

    feat: update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{bail, Result};
 use sqlite_starter_rust::{column::SerialValue, database::Database};
 
 fn main() -> Result<()> {
@@ -56,6 +56,42 @@ fn main() -> Result<()> {
                 }
                 None => eprintln!("can not read first page"),
             }
+        }
+        query if query.starts_with("SELECT") => {
+            let file_path = &args[1];
+            let target_table = query.split(" ").last().expect("specify table name");
+
+            let db = Database::read_file(file_path)?;
+            eprintln!("Number of page: {}", db.pages.len());
+            if let Some(first_page) = db.pages.get(0) {
+                for i in 0..db.tables() {
+                    if let Ok(record) = first_page.read_cell(i) {
+                        match record.columns[0].data() {
+                            SerialValue::String(ref str) => {
+                                if str != "table" {
+                                    continue;
+                                }
+                            }
+                            _ => {}
+                        }
+
+                        match record.columns[2].data() {
+                            SerialValue::String(str) => match str.as_str() {
+                                "sqlite_sequence" => {
+                                    continue;
+                                }
+                                target_table => {}
+                                _ => {}
+                            },
+
+                            _ => {}
+                        }
+                    }
+
+                    tables.push_str(&format!("{} ", tbl_name));
+                }
+            }
+            println!("{tables}");
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
