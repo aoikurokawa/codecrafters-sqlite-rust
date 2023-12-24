b78bffb62318e34800b364afce8a72bc8674f70e
aoikurokawa <ht.so.ik0102@gmail.com>
2023-12-24 12:43:08 +0900

    feat: use nom-sql

--- src/main.rs
+++ src/main.rs
@@ -59,58 +59,62 @@ fn main() -> Result<()> {
         }
         query if query.to_lowercase().starts_with("select") => {
             let file_path = &args[1];
-            let target_table = query.split(" ").last().expect("specify table name");
+            let sql_query = nom_sql::parse_query(query).expect("");
+            // let target_table = query.split(" ").last().expect("specify table name");
 
-            let db = Database::read_file(file_path)?;
-            // eprintln!("Number of page: {}", db.pages.len());
-            // for page in &db.pages {
-            //     eprintln!("{:?}", page.cell_offsets.len());
-            // }
-            if let Some(first_page) = db.pages.get(0) {
-                for i in 0..db.tables() {
-                    if let Ok(record) = first_page.read_cell(i) {
-                        // eprintln!("{:?}", record.columns.len());
-                        // eprintln!("{:?}", record.columns[0].data());
-                        // eprintln!("{:?}", record.columns[1].data());
-                        // eprintln!("{:?}", record.columns[2].data());
-                        // eprintln!("{:?}", record.columns[3].data());
-                        // eprintln!("{:?}", record.columns[4].data());
-
-                        match record.columns[0].data() {
-                            SerialValue::String(ref str) => {
-                                if str != "table" {
-                                    continue;
+            match sql_query {
+                nom_sql::SqlQuery::Select(select) => {
+                    let db = Database::read_file(file_path)?;
+                    if let Some(first_page) = db.pages.get(0) {
+                        for i in 0..db.tables() {
+                            if let Ok(record) = first_page.read_cell(i) {
+                                match record.columns[0].data() {
+                                    SerialValue::String(ref str) => {
+                                        if str != "table" {
+                                            continue;
+                                        }
+                                    }
+                                    _ => {}
                                 }
-                            }
-                            _ => {}
-                        }
 
-                        match record.columns[2].data() {
-                            SerialValue::String(str) => match str.as_str() {
-                                "sqlite_sequence" => {
-                                    continue;
-                                }
-                                table_name if table_name == target_table => {
-                                    // println!("{:?}", target_table);
-                                    match record.columns[3].data() {
-                                        SerialValue::I8(num) => {
-                                            // eprintln!("num: {num}");
-                                            if let Some(page) = db.pages.get(*num as usize - 1) {
-                                                println!("{:?}", page.cell_offsets.len());
+                                match record.columns[2].data() {
+                                    SerialValue::String(str) => match str.as_str() {
+                                        "sqlite_sequence" => {
+                                            continue;
+                                        }
+                                        t_name => {
+                                            for table_name in &select.tables {
+                                                // println!("{:?}", target_table);
+                                                if table_name.name == t_name {
+                                                    match record.columns[3].data() {
+                                                        SerialValue::I8(num) => {
+                                                            // eprintln!("num: {num}");
+                                                            if let Some(page) =
+                                                                db.pages.get(*num as usize - 1)
+                                                            {
+                                                                println!(
+                                                                    "{:?}",
+                                                                    page.cell_offsets.len()
+                                                                );
+                                                            }
+                                                        }
+                                                        _ => {}
+                                                    }
+                                                }
                                             }
                                         }
                                         _ => {}
-                                    }
-                                }
-                                _ => {}
-                            },
+                                    },
 
-                            _ => {}
+                                    _ => {}
+                                }
+                            }
+
+                            // tables.push_str(&format!("{} ", tbl_name));
                         }
                     }
-
-                    // tables.push_str(&format!("{} ", tbl_name));
                 }
+                _ => todo!(),
             }
             // println!("{tables}");
         }
