b18bf927329c3326044690e095d7d98a8ec0a80b
aoikurokawa <ht.so.ik0102@gmail.com>
2023-12-30 17:10:21 +0900

    style: format everything

--- src/database.rs
+++ src/database.rs
@@ -104,7 +104,6 @@ impl Database {
                 let cell_len = page.cell_offsets.len();
 
                 if !select_statement.selection.is_empty() {
-
                     let mut ids = ids;
 
                     for i in 0..cell_len {
